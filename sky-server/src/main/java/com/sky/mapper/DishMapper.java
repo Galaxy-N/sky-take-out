bb30ad1c1e1e86b83f15803bc76008fd4b2009e9
fsn <1355548621@qq.com>
2024-08-26 22:24:09 +0800

    wechat and dish view

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -68,4 +68,11 @@ public interface DishMapper {
 
     @Select("select * from dish where category_id = #{categoryId}")
     List<Dish> queryByCategoryId(Integer categoryId);
+
+    /**
+     * 动态条件查询菜品
+     * @param dish
+     * @return
+     */
+    List<Dish> list(Dish dish);
 }
---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("select * from user where openid = #{openid}")
+    User getByOpenid (String openid);
+
+    /**
+     * 向用户表中插入新数据
+     * @param user
+     */
+    void insert(User user);   // 这里需要返回主键值
+}
